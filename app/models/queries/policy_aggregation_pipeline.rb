b4a6d5c7e12c3d47a4623ba77ee432958d995f39
Trey Evans <lewis.r.evans@gmail.com>
2015-12-15 14:52:46 -0500

    More metric utilities.

---
+++ app/models/queries/policy_aggregation_pipeline.rb
@@ -0,0 +1,181 @@
+module Queries
+  class PolicyAggregationPipeline
+    def initialize
+      @pipeline = base_pipeline
+    end
+
+    def base_pipeline
+      [
+        { "$unwind" => "$households"},
+        { "$unwind" => "$households.hbx_enrollments"}
+      ]
+    end
+
+    def add(step)
+      @pipeline << step
+    end
+
+    def evaluate
+      Family.collection.raw_aggregate(@pipeline)
+    end
+
+    def count
+      list_of_hbx_ids.count
+    end
+
+    def denormalize
+      add({
+        "$project" => {
+          "_id" => "$households.hbx_enrollments.hbx_id",
+          "policy_purchased_at" => { 
+            "$dateToString" => {"format" => "%Y-%m-%d %H:%M:S",
+              "date" => {"$ifNull" => ["$households.hbx_enrollments.created_at", "$households.hbx_enrollments.submitted_at"] }}},
+          "policy_purchased_on" => {
+            "$dateToString" => {"format" => "%Y-%m-%d",
+                                "date" => { "$ifNull" => ["$households.hbx_enrollments.created_at", "$households.hbx_enrollments.submitted_at"] }
+          }
+          },
+          "policy_effective_on" => {
+            "$dateToString" => {"format" => "%Y-%m-%d",
+            "date" => "$households.hbx_enrollments.effective_on"}},
+          "enrollee_count" => {"$size" => {"$ifNull" => ["$households.hbx_enrollments.hbx_enrollment_members", []]}},
+          "market" => {"$cond" => ["$households.hbx_enrollments.consumer_role_id","SHOP","IVL"]},
+          "plan_id" => "$households.hbx_enrollments.plan_id"
+        }})
+      self
+    end
+
+    def filter_to_employers_hbx_ids(hbx_id_list)
+      orgs = Organization.where(:hbx_id => {"$in" => hbx_id_list})
+      benefit_group_ids = orgs.map(&:employer_profile).flat_map(&:plan_years).flat_map(&:benefit_groups).map(&:id)
+      add({
+          "$match" => {
+                "households.hbx_enrollments.benefit_group_id" => { "$in" => benefit_group_ids }
+                  }
+      })
+      self
+    end
+
+    def filter_to_employers_feins(fein_list)
+      orgs = Organization.where(:fein => {"$in" => fein_list})
+      benefit_group_ids = orgs.map(&:employer_profile).flat_map(&:plan_years).flat_map(&:benefit_groups).map(&:id)
+      add({
+          "$match" => {
+                "households.hbx_enrollments.benefit_group_id" => { "$in" => benefit_group_ids }
+                  }
+      })
+      self
+    end
+
+    def filter_to_active
+      add({
+        "$match" => {
+          "households.hbx_enrollments.plan_id" => { "$ne" => nil},
+          "households.hbx_enrollments.aasm_state" => { "$nin" => [
+            "shopping", "inactive", "coverage_canceled", "coverage_terminated"
+          ]}
+        }
+      })
+      self
+    end
+
+    def filter_to_individual
+      add({
+        "$match" => {
+          "households.hbx_enrollments.plan_id" => { "$ne" => nil},
+          "households.hbx_enrollments.consumer_role_id" => {"$ne" => nil},
+          "households.hbx_enrollments.aasm_state" => { "$nin" => [
+            "shopping", "inactive", "coverage_canceled", "coverage_terminated"
+          ]}
+        }
+      })
+      self
+    end
+
+    def with_effective_date(criteria)
+      add({
+        "$match" => {
+          "households.hbx_enrollments.effective_on" => criteria
+        }
+      })
+      self
+    end
+
+    def filter_to_shop
+      add({
+        "$match" => {
+          "households.hbx_enrollments.plan_id" => { "$ne" => nil},
+          "$or" => [
+            {"households.hbx_enrollments.consumer_role_id" => {"$exists" => false}},
+            {"households.hbx_enrollments.consumer_role_id" => nil}
+          ],
+          "households.hbx_enrollments.aasm_state" => { "$nin" => [
+            "shopping", "inactive", "coverage_canceled", "coverage_terminated"
+          ]}
+        }
+      })
+      self
+    end
+
+    def list_of_hbx_ids
+      add({
+        "$group" => {"_id" => "$households.hbx_enrollments.hbx_id"}
+      })
+      results = evaluate
+      results.map do |h|
+        h["_id"]
+      end
+    end
+
+    def hbx_id_with_purchase_date_and_time
+      add({
+        "$project" => {
+          "policy_purchased_at" => { "$ifNull" => ["$households.hbx_enrollments.created_at", "$households.hbx_enrollments.submitted_at"] },
+          "policy_purchased_on" => {
+            "$dateToString" => {"format" => "%Y-%m-%d",
+                                "date" => { "$ifNull" => ["$households.hbx_enrollments.created_at", "$households.hbx_enrollments.submitted_at"] }
+          }
+          },
+          "hbx_id" => "$households.hbx_enrollments.hbx_id"
+        }})
+      yield self if block_given?
+      results = self.evaluate
+      results.map do |r|
+        r['hbx_id']
+      end
+    end
+
+    def group_by_purchase_date
+      add({
+        "$project" => {
+          "policy_purchased_at" => { "$ifNull" => ["$households.hbx_enrollments.created_at", "$households.hbx_enrollments.submitted_at"] },
+          "policy_purchased_on" => {
+            "$dateToString" => {"format" => "%Y-%m-%d",
+                                "date" => { "$ifNull" => ["$households.hbx_enrollments.created_at", "$households.hbx_enrollments.submitted_at"] }
+          }
+          }
+        }})
+      yield self if block_given?
+      add({
+        "$group" => {"_id" => {"purchased_on" => "$policy_purchased_on"}, "count" => {"$sum" => 1}}
+      })
+      results = self.evaluate
+      h = results.inject({}) do |acc,r|
+        k = r["_id"]["purchased_on"]
+        if acc.has_key?(k)
+          acc[k] = acc[k] + r["count"]
+        else
+          acc[k] = r["count"]
+        end
+        acc
+      end
+      result = h.keys.sort.map do |k|
+        [k, h[k]]
+      end
+      total = result.inject(0) do |acc, i|
+        acc + i.last
+      end
+      result << ["Total     ", total] 
+    end
+  end
+end
--- script/metrics_1215.rb
+++ script/metrics_1215.rb
@@ -1,35 +1,19 @@
 qi = Queries::PolicyAggregationPipeline.new
-qs = Queries::PolicyAggregationPipeline.new
-qs_binned = Queries::PolicyAggregationPipeline.new
-
 qi.filter_to_individual.with_effective_date({"$gt" => Date.new(2015,12,31), "$lt" => Date.new(2016,1,2)})
-qs.filter_to_shop.with_effective_date({"$gt" => Date.new(2015,12,31), "$lt" => Date.new(2016,1,2)})
 
 puts "Individual 1/1:"
 puts qi.count
 
-
 cong_hbx_ids = ["100101","100102","118510"]
 
-orgs = Organization.where(:hbx_id => {"$in" => cong_hbx_ids})
-
-benefit_group_ids = orgs.map(&:employer_profile).flat_map(&:plan_years).flat_map(&:benefit_groups).map(&:id)
-qs.add({
-  "$match" => {
-    "households.hbx_enrollments.benefit_group_id" => { "$in" => benefit_group_ids }
-  }
-})
+qs = Queries::PolicyAggregationPipeline.new
+qs.filter_to_employers_hbx_ids(cong_hbx_ids).filter_to_shop.with_effective_date({"$gt" => Date.new(2015,12,31), "$lt" => Date.new(2016,1,2)})
 
 puts "Congress 1/1:"
 puts qs.count
 
 qs_binned = Queries::PolicyAggregationPipeline.new
-qs_binned.filter_to_shop.with_effective_date({"$gt" => Date.new(2015,12,31), "$lt" => Date.new(2016,1,2)})
-qs_binned.add({
-  "$match" => {
-    "households.hbx_enrollments.benefit_group_id" => { "$in" => benefit_group_ids }
-  }
-})
+qs_binned.filter_to_employers_hbx_ids(cong_hbx_ids).filter_to_shop.with_effective_date({"$gt" => Date.new(2015,12,31), "$lt" => Date.new(2016,1,2)})
 
 puts "Congress 1/1 enrollment by day:"
 qs_binned.group_by_purchase_date.each do |rec|
