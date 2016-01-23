af78e7ff74319fb5a3b41cc1d70100097fc3bea9
Zoheb <Zoheb2@zohebneomputer2.westell.com>
2016-01-23 18:47:58 -0500

    Ref 3421 Adding line to find all users who have an employee role, not just people

--- script/find_unlinked_employees.rb
+++ script/find_unlinked_employees.rb
@@ -1,5 +1,9 @@
+
+## Find all people who have an employee role. 
+all_employee_users = User.where(:roles => {"$in" => ["employee"]})
 all_employee_persons = Person.where(:employee_roles => {"$ne" => nil})
 
+## This goes through and checks each of these people to see if they are currently enrolled 
 all_employee_persons.each do |employee_person|
 	census_emp = employee_person.employee_roles.first.census_employee
 	if census_emp.aasm_state == "eligible"
