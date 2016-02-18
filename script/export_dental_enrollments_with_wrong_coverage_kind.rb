79f14be42b0b873c3fb0a43f3be12b4f7f5c51f4
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-02-18 14:40:31 -0500

    Ref #5197 - changed comment

--- script/export_dental_enrollments_with_wrong_coverage_kind.rb
+++ script/export_dental_enrollments_with_wrong_coverage_kind.rb
@@ -1,17 +1,16 @@
 # ticket https://devops.dchbx.org/redmine/issues/5142
-# this script will export family, hbx_enrollment, plan details for dental plans mistakenly shopped in shop
+# this script will export family, hbx_enrollment, plan details for dental plans having coverage kind as health
 
 csv = CSV.open("dental_plans_in_shop.csv", "w")
 csv << %w(hbx_enrollment.household.family.id, hbx_enrollment.hbx_id, hbx_enrollment.plan.name,
           hbx_enrollment.coverage_kind, hbx_enrollment.plan.coverage_kind hbx_enrollment.kind hbx_enrollment.created_at
-          hbx_enrollment.aasm_state subscriber)
+          hbx_enrollment.aasm_state subscriber is_active?)
 
 batch_size = Family.count/10
 offset = 0
 
 while offset < Family.count
   Family.offset(offset).limit(batch_size).flat_map(&:households).flat_map(&:hbx_enrollments).each do |hbx_enrollment|
-    next unless hbx_enrollment.is_active
     begin
       if hbx_enrollment.coverage_kind != hbx_enrollment.plan.coverage_kind
         if hbx_enrollment.subscriber
@@ -23,10 +22,10 @@ while offset < Family.count
 
         csv << [hbx_enrollment.household.family.id, hbx_enrollment.hbx_id,
                 hbx_enrollment.plan.name, hbx_enrollment.coverage_kind, hbx_enrollment.plan.coverage_kind,
-                hbx_enrollment.kind, hbx_enrollment.created_at, hbx_enrollment.aasm_state, subscriber]
+                hbx_enrollment.kind, hbx_enrollment.created_at, hbx_enrollment.aasm_state, subscriber, hbx_enrollment.is_active]
       end
     rescue Exception => e
-
+      puts "#{hbx_enrollment.household.family.id} #{hbx_enrollment.hbx_id} " + e.message
     end
   end
   offset += batch_size
