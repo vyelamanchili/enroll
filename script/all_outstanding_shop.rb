a6f28a90323afac38747f961a42740aa73f048ba
Trey Evans <lewis.r.evans@gmail.com>
2016-02-22 13:32:32 -0500

    Cleanup of shop query.

--- script/all_outstanding_shop.rb
+++ script/all_outstanding_shop.rb
@@ -1,6 +1,6 @@
 qs = Queries::PolicyAggregationPipeline.new
 
-qs.filter_to_shop.filter_to_active.with_effective_date({"$gt" => Date.new(2015,9,30)}).eliminate_family_duplicates
+qs.filter_to_shop.filter_to_active.with_effective_date({"$gt" => Date.new(2015,1,31)}).eliminate_family_duplicates
 
 enroll_pol_ids = []
 
