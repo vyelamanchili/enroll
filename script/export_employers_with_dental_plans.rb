5ac28b24c3063b932eee5a70d120f3f59268ca33
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-02-23 10:25:55 -0500

    Refs #5142 - export employers with dental plans in plan years

---
+++ script/export_employers_with_dental_plans.rb
@@ -0,0 +1,18 @@
+# ticket https://devops.dchbx.org/redmine/issues/5142
+# this script will export employers with dental plans in plan_years
+
+organizations = Organization.where(:employer_profile.exists => true)
+
+organizations.each do |org|
+  org.employer_profile.plan_years.flat_map(&:benefit_groups).each do |benefit_group|
+    benefit_group.elected_plans.each do |elected_plan|
+      begin
+        if elected_plan.coverage_kind == 'dental'
+          puts "#{org.employer_profile.legal_name}, #{org.employer_profile.fein}, #{benefit_group.title}, #{elected_plan.name}, #{benefit_group.plan_year.start_on}"
+        end
+      rescue Exception => e
+        puts "#{org.id} " + e.message
+      end
+    end
+  end
+end
