1289c05bcb524ffeaced5b1404ee069472b269b7
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-02-24 12:06:38 -0500

    Refs #5142 - script to export employers with dental plans in plan years

--- script/export_employers_with_dental_plans.rb
+++ script/export_employers_with_dental_plans.rb
@@ -1,6 +1,9 @@
 # ticket https://devops.dchbx.org/redmine/issues/5142
 # this script will export employers with dental plans in plan_years
 
+csv = CSV.open("employers_with_dental_plans_in_planyears.csv", "w")
+csv << %w(org.employer_profile.legal_name, org.employer_profile.fein, benefit_group.title, elected_plan.name,
+             elected_plan.hios_id, benefit_group.plan_year.start_on)
 organizations = Organization.where(:employer_profile.exists => true)
 
 organizations.each do |org|
@@ -8,7 +11,8 @@ organizations.each do |org|
     benefit_group.elected_plans.each do |elected_plan|
       begin
         if elected_plan.coverage_kind == 'dental'
-          puts "#{org.employer_profile.legal_name}, #{org.employer_profile.fein}, #{benefit_group.title}, #{elected_plan.name},  #{elected_plan.hios_id}, #{benefit_group.plan_year.start_on}"
+          csv << [org.employer_profile.legal_name, org.employer_profile.fein, benefit_group.title, elected_plan.name,
+            elected_plan.hios_id, benefit_group.plan_year.start_on]
         end
       rescue Exception => e
         puts "#{org.id} " + e.message
