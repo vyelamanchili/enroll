f70d93674ddeec67614811bfd6d444962d98a7de
Varun Reddy Battula <Varun@Varun-2.local>
2016-02-29 09:25:45 -0500

    Refs#2830 Updated user SEP steps

--- features/insured/step_definitions/user_sep_steps.rb
+++ features/insured/step_definitions/user_sep_steps.rb
@@ -8,34 +8,35 @@ end
 World(UserSepWorld)
 
 Given(/^a user exists$/) do
-  user :consumer, :with_family
+  user :consumer, :with_family, :ridp_verified
   @browser.goto("http://localhost:3000/users/sign_in")
-  puts @user.password
 end
 
 Given(/^the user signs in$/) do
   @browser.element(text: /Sign In/i).wait_until_present
-  @browser.text_field(class: /interaction-field-control-user-email/).set(@user.email)
-  @browser.text_field(class: /interaction-field-control-user-password/).set(@user.password)
+  @browser.text_field(class: /interaction-field-control-user-email/).set(user.email)
+  @browser.text_field(class: /interaction-field-control-user-password/).set(user.password)
   @browser.element(class: /interaction-click-control-sign-in/).click
 end
 
 Given(/^has already enrolled for SEP$/) do
-  @user.primary_family.special_enrollment_periods.push FactoryGirl.build(:special_enrollment_period)      
+  FactoryGirl.create :hbx_profile
+  user.person.consumer_role = FactoryGirl.build(:consumer_role, person: user.person, aasm_state: 'fully_verified')
+  user.person.save
+  user.primary_family.special_enrollment_periods.push FactoryGirl.build(:special_enrollment_period, qle_on: Date.yesterday, effective_on_kind: ['first_of_month'])
+  user.primary_family.save
 end
 
 When(/^the user goes to the families homepage$/) do
   @browser.goto("http://localhost:3000/families/home?tab=home")
-  @browser.element(class: /interaction-click-control-shop-for-plans/).wait_until_present
- scroll_then_click(@browser.element(class: /interaction-click-control-shop-for-plans/))
 end
 
 When(/^the user clicks Shop for plans button$/) do
-  sleep 30
-  @browser.element(class: /interaction-click-control-shop-for-plans/).click
+  @browser.element(class: /interaction-click-control-shop-for-plans/).wait_until_present
+  scroll_then_click(@browser.element(class: /interaction-click-control-shop-for-plans/))
 end
 
-Then(/^SEP popup should not appear$/) do
-  sleep 5
-  expect(@browser.div(class: "seps-panel").visible?).to be_false
+Then(/^the user should be able to use their existing SEP$/) do
+  sleep 1
+  expect(@browser.a(text: /Shop with existing SEP/i).visible?).to be_truthy
 end
