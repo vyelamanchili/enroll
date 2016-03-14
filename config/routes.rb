fdb5399faf7cd3cd7a3918b76201cbe983e0fe71
Ken Lu <ken@seeice.com>
2016-03-14 08:40:37 -0400

    Refs #5485 - Secure Mailbox from HBXAdmin to and from General Agency

--- config/routes.rb
+++ config/routes.rb
@@ -273,8 +273,11 @@ Rails.application.routes.draw do
     root 'profiles#new'
     resources :profiles do
       collection do
+        get :new_agency_staff
         get :new_agency
         get :messages
+        get :agency_messages
+        get :inbox
         get :edit_staff
         post :update_staff
       end
@@ -283,6 +286,9 @@ Rails.application.routes.draw do
         get :families
         get :staffs
       end
+    end
+    resources :inboxes, only: [:new, :create, :show, :destroy] do
+      get :msg_to_portal
     end
   end
 
