399acc7977a2b346d513ffa759845c47421b25c1
Raghuram <raghuramg83@gmail.com>
2016-04-07 09:54:54 -0400

    Refs #5700 updates for the script

--- script/reinstate_employees.rb
+++ script/reinstate_employees.rb
@@ -1,20 +1,14 @@
-# This script requires a text file with a list of policy IDs. 
+puts "Please enter enrollment group ids of the policies"
+hbx_ids = gets.chomp.to_s.split(',')
 
-policy_ids = []
-
-File.readlines('policies_to_reinstate.txt').map do |line|
-	policy_ids.push(line.to_s)
-end
-
-policy_ids.each do |id|
-	family = Family.where("households.hbx_enrollments.hbx_id" => id.to_s)
+hbx_ids.each do |id|
+	family = Family.where("households.hbx_enrollments.hbx_id" => id.to_s).first
 	family.households.each do |household|
 		household.hbx_enrollments.each do |hbx_enrollment|
 			if hbx_enrollment.hbx_id == id.to_s
-				hbx_enrollment.update_attributes({:aasm_state => 'coverage_enrolled'})
+				hbx_enrollment.update_attributes({:aasm_state => 'coverage_enrolled'}) 
 				hbx_enrollment.benefit_group_assignment.make_active
-				hbx_enrollment.benefit_group_assignment.select_coverage!
-				hbx_enrollment.save!
+				hbx_enrollment.benefit_group_assignment.select_coverage! if hbx_enrollment.benefit_group_assignment.may_select_coverage?
 			end
 		end
 	end
