6616618f982ec2427f57d1668fb2213406cd5ea0
jfinucane <jimway@gmail.com>
2016-04-21 21:19:12 -0400

    turbo employer costs

---
+++ app/assets/javascripts/quotes/set_plan_costs.js
@@ -0,0 +1,21 @@
+function set_plan_costs() {
+    var plan_ids = Object.keys(window.roster_premiums)
+    for(var i = 0; i< plan_ids.length; i++){
+      premium = 0
+      plan_id = plan_ids[i]
+      premiums = window.roster_premiums[plan_ids[i]]
+      kinds = Object.keys(premiums) 
+      for (var j=0; j<kinds.length; j++) {
+      	kind = kinds[j]
+        premium = premium + premiums[kind] *  window.relationship_benefits[kind]
+
+      }
+     premium = Math.round(premium)/100.
+     plan_button = "[value='" + plan_id + "']"
+     window.plan_button = plan_button
+     employee_cost_div = $(plan_button).parent().children()[1]
+     $(employee_cost_div).html(premium)
+
+    }
+
+}
