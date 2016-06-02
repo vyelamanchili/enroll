12e0b8cf407c380b81770fc936bfc4e1a68ec562
lisyk <sergey.liskovoy@gmail.com>
2016-06-02 17:58:32 -0400

    Refs #7634 fix script for only members without role

--- script/consumer_role_for_fam_mem.rb
+++ script/consumer_role_for_fam_mem.rb
@@ -7,18 +7,20 @@ broken_family=Family.find("566ebebcfaca145b8900000c")
 
 families.each do |family|
   unless family == broken_family do
-    if family.primary_applicant.person.consumer_role
+    if family.primary_applicant.person.consumer_role.present?
       family.family_members.each do |family_member|
-        begin
-          person = family_member
-          dob = person.dob
-          gender = person.gender
-          is_applicant = person.is_applicant
-          consumer_role = ConsumerRole.new(:dob => dob, :gender => gender, :is_applicant => is_applicant )
-          person.consumer_role = consumer_role
-          person.save!
-        rescue => e
-          puts "This person #{person.id} can't be updated. Error #{e.message}"
+        unless family_member.person.consumer_role.present?
+          begin
+            person = family_member
+            dob = person.dob
+            gender = person.gender
+            is_applicant = person.is_applicant
+            consumer_role = ConsumerRole.new(:dob => dob, :gender => gender, :is_applicant => is_applicant )
+            person.consumer_role = consumer_role
+            person.save!
+          rescue => e
+            puts "This person #{person.id} can't be updated. Error #{e.message}"
+          end
         end
       end
     end
