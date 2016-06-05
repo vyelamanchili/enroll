f0cf5c960a3af40451e1f5d515bcc46c5783c857
jfinucane <jimway@gmail.com>
2016-06-05 11:39:34 -0400

    consistent use of ajax for benefit comparison

--- app/assets/javascripts/quotes/compare_plans.js
+++ app/assets/javascripts/quotes/compare_plans.js
@@ -1,8 +1,17 @@
- sort_plans = function(){
+
+selected_plans = function(){
+  var plans=[];
+  $.each($('.btn.active input'), function(i,item){plans.push( $(item).attr("value"))})
+  return(plans)
+}
+
+sort_plans = function(){
       var sort_by = $(this.parentNode).text();
-      console.log('clicked', sort_by)
-      var plans=[];
-      $.each($('.btn.active input'), function(i,item){plans.push( $(item).attr("value"))})
+      var plans = selected_plans()
+      if(plans.length == 0) {
+        alert('Please select one or more plans for comparison');
+        return;
+      }
       $.ajax({
         type: "GET",
         url: "/broker_agencies/quotes/plan_comparison",
@@ -10,6 +19,7 @@
         success: function(response) {
           $('#plan_comparison_frame').html(response);
           compare_plans_listeners();
+          collapse_all()
         }
       })
     }
