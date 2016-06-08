be2f09e08caa67b21a9f7d55d5194aa05852baac
Phillip Jacowski <phillipjacowski@gmail.com>
2016-06-08 16:38:01 -0400

    resolve conflict in spec/views/insured/employee_roles/no_match.html.haml_spec.rb

--- spec/views/insured/employee_roles/no_match.html.haml_spec.rb
+++ spec/views/insured/employee_roles/no_match.html.haml_spec.rb
@@ -14,13 +14,7 @@ RSpec.describe "insured/employee_roles/no_match.html.haml" do
     expect(rendered).to have_selector('h1', text: 'Personal Information')
     expect(rendered).to have_selector("input[type='text']", count: 5)
     expect(rendered).to have_selector("input[type='radio']", count: 2)
-
-<<<<<<< HEAD
-    expect(rendered).to have_selector('strong', text: 'No employer plan found.')
-    expect(rendered).to have_selector('div', text: "Check your personal information and try again OR contact " + Settings.contact_center.name + ": " + Settings.contact_center.phone_number)
-=======
     expect(rendered).to have_selector('strong', text: 'No employer found.')
     expect(rendered).to have_selector('div', text: "Please check the information entered above and confirm with your employer that your demographic information is listed correctly on their roster. For further assistance, please contact #{Settings.contact_center.name}: #{Settings.contact_center.phone_number}.")
->>>>>>> master
   end
 end
