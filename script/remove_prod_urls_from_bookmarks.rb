08922d54aa04a61f7f786d3b5e6f0be859a13028
Kevin Wei <kevin.tw.wei@gmail.com>
2016-06-09 15:03:43 -0400

    Refs# 3947 script update

--- script/remove_prod_urls_from_bookmarks.rb
+++ script/remove_prod_urls_from_bookmarks.rb
@@ -1,17 +1,7 @@
-p = Person.where(is_active:true)
-
-p.each do |c|
-  if c.present?
-    if c.consumer_role.present?
-      if c.consumer_role.bookmark_url.present?
-        link = c.consumer_role.bookmark_url.gsub("https://enroll.dchealthlink.com", '')
-        c.consumer_role.update_attributes!(bookmark_url:link)
-        p c.consumer_role.bookmark_url
-      end
-    end
+Person.all.no_timeout.each do |c|
+  if c.present? && c.consumer_role.present? && c.consumer_role.bookmark_url.present? && c.consumer_role.bookmark_url.include?("https://enroll.dchealthlink.com")
+    link = c.consumer_role.bookmark_url.gsub("https://enroll.dchealthlink.com", '')
+    c.consumer_role.update_attributes!(bookmark_url: link)
+    p c.consumer_role.bookmark_url
   end
 end
-
-
-
-
