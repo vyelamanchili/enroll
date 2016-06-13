c49b2a4d58a5fc78d45dcee5676ab194bd4180a3
je110 <justinellis110@gmail.com>
2016-06-13 12:48:03 -0400

    Refs #7774 - roster quoting tool - Dental UI

--- config/initializers/plans_cache.rb
+++ config/initializers/plans_cache.rb
@@ -9,12 +9,16 @@ Plan::REFERENCE_PLAN_METAL_LEVELS.map do |metal_level|
 end
 include Acapi::Notifiers
 $quote_shop_health_plans = Plan.shop_health_by_active_year(2016).all.entries
+$quote_shop_dental_plans = Plan.shop_dental_by_active_year(2016).all.entries
 
 def build_plan_selectors market_kind='shop', coverage_kind='health'
-  #TODO extend to Dental
-  plans = $quote_shop_health_plans
+  plans = coverage_kind == 'health' ? $quote_shop_health_plans : $quote_shop_dental_plans
   selectors = {}
-  selectors[:metals] =      plans.map{|p| p.metal_level}.uniq.append('any')
+  if coverage_kind == 'dental'
+    selectors[:dental_levels] =      plans.map{|p| p.dental_level}.uniq.append('any')
+  else
+    selectors[:metals] =      plans.map{|p| p.metal_level}.uniq.append('any')
+  end
   selectors[:carriers] =    plans.map{|p|
     [ p.carrier_profile.legal_name, p.carrier_profile.abbrev ]
     }.uniq.append(['any','any'])
@@ -25,14 +29,17 @@ def build_plan_selectors market_kind='shop', coverage_kind='health'
 end
 
 def build_plan_features market_kind='shop', coverage_kind='health'
-  #TODO Extend to Dental
-  plans = $quote_shop_health_plans
+  plans = coverage_kind == 'health' ? $quote_shop_health_plans : $quote_shop_dental_plans
   feature_array = []
   plans.each{|plan|
 
     characteristics = {}
     characteristics['plan_id'] = plan.id.to_s
-    characteristics['metal'] = plan.metal_level
+    if coverage_kind == 'dental'
+      characteristics['dental_level'] = plan.dental_level
+    else
+      characteristics['metal'] = plan.metal_level
+    end
     characteristics['carrier'] = plan.carrier_profile.organization.legal_name
     characteristics['plan_type'] = plan.plan_type
     characteristics['deductible'] = plan.deductible_int
@@ -51,3 +58,5 @@ end
 
 $quote_shop_health_selectors = build_plan_selectors
 $quote_shop_health_plan_features = build_plan_features
+$quote_shop_dental_selectors = build_plan_selectors('shop', 'dental')
+$quote_shop_dental_plan_features = build_plan_features('shop', 'dental')
