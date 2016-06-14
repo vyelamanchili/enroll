044636307176015f15c5624d0a879f0c2588b502
jfinucane <jimway@gmail.com>
2016-06-14 15:41:22 -0400

    Ajax for cost comparison

---
+++ app/assets/javascripts/quotes/compare_cost.js
@@ -0,0 +1,35 @@
+function get_health_cost_comparison() {
+  plans = selected_plans()
+  quote_id=$('#quote').val()
+  if(plans.length == 0) {
+    alert('Please select one or more plans for comparison');
+    return;
+   }
+  $.ajax({
+    type: "GET",
+    url: "/broker_agencies/quotes/health_cost_comparison",
+    data: {plans: plans, quote: quote_id},
+    success: function(response) {
+      $('#plan_comparison_frame').html(response);
+
+    }
+  })
+}
+
+function get_dental_cost_comparison() {
+  plans = selected_plans()
+  quote_id=$('#quote').val()
+  if(plans.length == 0) {
+    alert('Please select one or more plans for comparison');
+    return;
+   }  
+  $.ajax({
+    type: "GET",
+    url: "/broker_agencies/quotes/dental_cost_comparison",
+    data: {plans: plans, quote: quote_id},
+    success: function(response) {
+      $('#dental_plan_comparison_frame').html(response);
+
+    }
+  })
+}
