c7ffc2bd9dca9938c29b6289e3f2ead0b6442855
je110 <justinellis110@gmail.com>
2016-06-21 10:40:09 -0400

    cherry-pick 8ec6fe6 merge

--- app/assets/javascripts/quotes/set_plan_costs.js
+++ app/assets/javascripts/quotes/set_plan_costs.js
@@ -19,3 +19,23 @@ function set_plan_costs() {
     }
 
 }
+
+function set_dental_plan_costs() {
+  var plan_ids = Object.keys(window.dental_roster_premiums)
+  for(var i = 0; i< plan_ids.length; i++){
+    premium = 0
+    plan_id = plan_ids[i]
+    premiums = window.dental_roster_premiums[plan_ids[i]]
+    kinds = Object.keys(premiums) 
+    for (var j=0; j<kinds.length; j++) {
+      kind = kinds[j]
+      premium = premium + premiums[kind] * window.dental_relationship_benefits[kind]
+    }
+    premium = Math.round(premium)/100.
+    plan_button = "[value='" + plan_id + "']"
+    window.plan_button = plan_button
+    employee_cost_div = $(plan_button).parent().children()[1]
+    $(employee_cost_div).html(Math.ceil(parseFloat(premium)))
+  }
+}
+
