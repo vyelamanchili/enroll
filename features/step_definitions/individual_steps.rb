aafc3039300054fae654959081fddd4b0e44cfc1
Phillip Jacowski <phillipjacowski@gmail.com>
2016-06-30 19:24:08 -0500

    refs #8057 replace help me sign up widget with new broker/assister widget

--- features/step_definitions/individual_steps.rb
+++ features/step_definitions/individual_steps.rb
@@ -269,7 +269,7 @@ Then(/^Second user should see a form to enter personal information$/) do
 end
 
 Then(/Individual asks for help$/) do
-  find(:xpath, '/html/body/div[2]/div[2]/div/div[2]/div[2]').click
+  click_link "Get Help Signing Up"
   sleep 1
   click_link "Help from a Customer Service Representative"
   sleep 1
