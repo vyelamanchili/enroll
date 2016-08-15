b3f047f3ba1c82396b648d54f415cb111827f061
Ranjeesh Yelamanchili <php.ranjeesh@gmail.com>
2016-08-15 16:09:22 -0400

    Gender Radio buttons issue fixed.

--- app/assets/javascripts/consumer_role.js
+++ app/assets/javascripts/consumer_role.js
@@ -11,3 +11,23 @@ $(document).on('change', "#person_no_dc_address, #dependent_no_dc_address, #no_d
     $(this).parents('#address_info').find('.address_required').attr('required', true);
   };
 });
+
+//Code to unmark or mark SSN field depending on checkbox selection on application.
+$( document ).ready(function() { 
+   
+    if ($("#person_no_ssn").is(':checked')) {
+        $('#person_ssn').prop('required', false);
+    }
+    else {
+        $('#person_ssn').prop('required', true);
+    }
+    
+    $("#person_no_ssn").change( function() {
+        if(this.checked) {
+           $('#person_ssn').prop('required', false);
+       } 
+       else {
+           $('#person_ssn').prop('required', true);
+       }
+    });  
+});
